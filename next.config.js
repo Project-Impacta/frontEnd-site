953afe5a2dc6aa1b40de079e70d7f6b265b13e7a
Jonathan Souza Lima <jon.1lima@hotmail.com>
2024-02-28 23:55:11 -0300

    ajuste 2

--- next.config.js
+++ next.config.js
@@ -5,10 +5,9 @@ const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    unoptimized: false,
   },
   basePath: '/github-pages',
-  assetPrefix: '/github-pages/',
   distDir: 'build',
 }
 
