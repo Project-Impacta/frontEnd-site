34ff714797b97d5216cbe822434d7cbfc0aed9a9
Gabriel Cardoso Tavares <gabriel.tavares@actdigital.com>
2024-03-09 13:23:21 -0300

    config next and docker compose

--- next.config.js
+++ next.config.js
@@ -4,20 +4,39 @@ const path = require('path')
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  images: {
-    // Se você realmente precisa desativar a otimização de imagens (não recomendado),
-    // você pode definir `unoptimized: true`. Caso contrário, é melhor remover essa linha
-    // e deixar o Next.js otimizar suas imagens automaticamente.
-    unoptimized: true,
-  },
-  basePath: '/github-pages',
-  distDir: 'build',
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
-  },
-}
 
-module.exports = nextConfig
+if(process.env.NEXT_PUBLIC_NODE_ENV=='local'){
+  console.log('Ambiente local vai ser iniciado!')
+  const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      // Se você realmente precisa desativar a otimização de imagens (não recomendado),
+      // você pode definir `unoptimized: true`. Caso contrário, é melhor remover essa linha
+      // e deixar o Next.js otimizar suas imagens automaticamente.
+      unoptimized: true,
+    },
+    basePath: '/github-pages',
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'src/styles')],
+    },
+  }
+  module.exports = nextConfig
+} else {
+  console.log('FrontEnd-NextConfig-CICD')
+  const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    images: {
+      // Se você realmente precisa desativar a otimização de imagens (não recomendado),
+      // você pode definir `unoptimized: true`. Caso contrário, é melhor remover essa linha
+      // e deixar o Next.js otimizar suas imagens automaticamente.
+      unoptimized: true,
+    },
+    basePath: '/github-pages',
+    distDir: 'build',
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'src/styles')],
+    },
+  }
+  module.exports = nextConfig
+}
