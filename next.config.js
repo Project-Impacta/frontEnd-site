13175abe11c4886a4b9246e4ad6bdfa7da201e9a
Jonathan Souza Lima <jon.1lima@hotmail.com>
2024-03-10 17:48:25 -0300

    teste nextconfig2

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const path = require('path')
  * @type {import('next').NextConfig}
  */
 
-if (process.env.NEXT_PUBLIC_NODE_ENV != 'local') {
+if (process.env.NEXT_PUBLIC_NODE_ENV !== 'local') {
   console.log('FrontEnd-NextConfig-CI/CD')
   const nextConfig = {
     output: 'export',
