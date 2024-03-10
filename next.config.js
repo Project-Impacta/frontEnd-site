ec6e2b32047331704204cc6bfec6ccce8715ed5d
Jonathan Souza Lima <61327152+JhowSL@users.noreply.github.com>
2024-03-10 20:20:26 -0300

    Atualizar o next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const path = require('path')
  * @type {import('next').NextConfig}
  */
 
-// if (process.env.NEXT_PUBLIC_NODE_ENV !== 'local') {
+if (process.env.NEXT_PUBLIC_NODE_ENV !== 'local') {
 console.log('FrontEnd-NextConfig-CI/CD')
 const nextConfig = {
   output: 'export',
@@ -20,16 +20,18 @@ const nextConfig = {
   },
 }
 module.exports = nextConfig
-// } else {
-//   console.log('Ambiente local vai ser iniciado!')
-//   const nextConfig = {
-//     reactStrictMode: true,
-//     images: {
-//       unoptimized: true,
-//     },
-//     sassOptions: {
-//       includePaths: [path.join(__dirname, 'src/styles')],
-//     },
-//   }
-//   module.exports = nextConfig
-// }
+ } else {
+   console.log('Ambiente local vai ser iniciado!')
+   const nextConfig = {
+     reactStrictMode: true,
+     images: {
+       unoptimized: true,
+     },
+     basePath: '/github-pages',
+     distDir: 'build',
+     sassOptions: {
+       includePaths: [path.join(__dirname, 'src/styles')],
+     },
+   }
+   module.exports = nextConfig
+ }
