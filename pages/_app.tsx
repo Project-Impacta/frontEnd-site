38f65cc07775108bacfa5147dd6414e96a37ab20
Jonathan Souza Lima <jon.1lima@hotmail.com>
2024-03-10 17:31:36 -0300

    ajuste context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,15 @@
 import '../src/styles/tailwind.css'
 import Layout from '@/components/layout'
-import { AuthProvider, ThemeProvider } from '@/providers'
+import { ThemeProvider } from '@/providers'
 import type { AppProps } from 'next/app'
 import React from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <ThemeProvider attribute="class">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </AuthProvider>
+    <ThemeProvider attribute="class">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   )
 }
--- pages/home-page/index.tsx
+++ pages/home-page/index.tsx
@@ -1,6 +1,11 @@
 import { HomePage } from '@/components/landing-pages'
+import { AuthProvider } from '@/providers'
 import React from 'react'
 
 export default function Home(): JSX.Element {
-  return <HomePage />
+  return (
+    <AuthProvider>
+      <HomePage />
+    </AuthProvider>
+  )
 }
