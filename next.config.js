a394f8b478431f7fe3f3829f5c88a1e480588500
Jonathan Souza Lima <jon.1lima@hotmail.com>
2024-03-10 19:09:16 -0300

    roolback

--- next.config.js
+++ next.config.js
@@ -5,37 +5,31 @@ const path = require('path')
  * @type {import('next').NextConfig}
  */
 
-if (process.env.NEXT_PUBLIC_NODE_ENV !== 'local') {
-  console.log('FrontEnd-NextConfig-CI/CD')
-  const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/github-pages',
-    distDir: 'build',
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'src/styles')],
-    },
-  }
-  console.log(
-    `${nextConfig.output}\n${nextConfig.reactStrictMode}\n${nextConfig.images}\n${nextConfig.images.unoptimized}\n${nextConfig.basePath}\n${nextConfig.distDir}\n${nextConfig.sassOptions}\n${nextConfig.sassOptions.includePaths}`,
-  )
-  module.exports = nextConfig
-} else {
-  console.log('Ambiente local vai ser iniciado!')
-  const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      unoptimized: true,
-    },
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'src/styles')],
-    },
-  }
-  console.log(
-    `${nextConfig.reactStrictMode}\n${nextConfig.images}\n${nextConfig.sassOptions}\n${nextConfig.sassOptions.includePaths}`,
-  )
-  module.exports = nextConfig
+// if (process.env.NEXT_PUBLIC_NODE_ENV !== 'local') {
+console.log('FrontEnd-NextConfig-CI/CD')
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/github-pages',
+  distDir: 'build',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
 }
+module.exports = nextConfig
+// } else {
+//   console.log('Ambiente local vai ser iniciado!')
+//   const nextConfig = {
+//     reactStrictMode: true,
+//     images: {
+//       unoptimized: true,
+//     },
+//     sassOptions: {
+//       includePaths: [path.join(__dirname, 'src/styles')],
+//     },
+//   }
+//   module.exports = nextConfig
+// }
