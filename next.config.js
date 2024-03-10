c7fd1e2e7a36a8f7efb572c62ae52cf0a847d673
Jonathan Souza Lima <jon.1lima@hotmail.com>
2024-03-10 20:40:15 -0300

    teste 1

--- next.config.js
+++ next.config.js
@@ -6,32 +6,32 @@ const path = require('path')
  */
 
 if (process.env.NEXT_PUBLIC_NODE_ENV !== 'local') {
-console.log('FrontEnd-NextConfig-CI/CD')
-const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/github-pages',
-  distDir: 'build',
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
-  },
+  console.log('FrontEnd-NextConfig-CI/CD')
+  const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/github-pages',
+    distDir: 'build',
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'src/styles')],
+    },
+  }
+  module.exports = nextConfig
+} else {
+  console.log('Ambiente local vai ser iniciado!')
+  const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/github-pages',
+    distDir: 'build',
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'src/styles')],
+    },
+  }
+  module.exports = nextConfig
 }
-module.exports = nextConfig
- } else {
-   console.log('Ambiente local vai ser iniciado!')
-   const nextConfig = {
-     reactStrictMode: true,
-     images: {
-       unoptimized: true,
-     },
-     basePath: '/github-pages',
-     distDir: 'build',
-     sassOptions: {
-       includePaths: [path.join(__dirname, 'src/styles')],
-     },
-   }
-   module.exports = nextConfig
- }
