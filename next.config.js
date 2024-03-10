623177f0eed1bc3cc1c365aa8ea96b325311a17b
Jonathan Souza Lima <jon.1lima@hotmail.com>
2024-03-10 17:41:00 -0300

    Revert "teste inversão logicade config no next.config" teste pipeline This reverts commit 3477b2e21e9df5adf6cc11de7c1ceb39e0b540a6.

--- next.config.js
+++ next.config.js
@@ -5,31 +5,38 @@ const path = require('path')
  * @type {import('next').NextConfig}
  */
 
-if (process.env.NEXT_PUBLIC_NODE_ENV !== 'local') {
-  console.log('FrontEnd-NextConfig-CI/CD')
-  const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/github-pages',
-    distDir: 'build',
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'src/styles')],
-    },
-  }
-  module.exports = nextConfig
-} else {
-  console.log('Ambiente local vai ser iniciado!')
-  const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      unoptimized: true,
-    },
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'src/styles')],
-    },
-  }
-  module.exports = nextConfig
+// if (process.env.NEXT_PUBLIC_NODE_ENV == 'local') {
+//   console.log('Ambiente local vai ser iniciado!')
+//   const nextConfig = {
+//     reactStrictMode: true,
+//     images: {
+//       // Se você realmente precisa desativar a otimização de imagens (não recomendado),
+//       // você pode definir `unoptimized: true`. Caso contrário, é melhor remover essa linha
+//       // e deixar o Next.js otimizar suas imagens automaticamente.
+//       unoptimized: true,
+//     },
+//     basePath: '/github-pages',
+//     sassOptions: {
+//       includePaths: [path.join(__dirname, 'src/styles')],
+//     },
+//   }
+//   module.exports = nextConfig
+// } else {
+console.log('FrontEnd-NextConfig-CICD')
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  images: {
+    // Se você realmente precisa desativar a otimização de imagens (não recomendado),
+    // você pode definir `unoptimized: true`. Caso contrário, é melhor remover essa linha
+    // e deixar o Next.js otimizar suas imagens automaticamente.
+    unoptimized: true,
+  },
+  basePath: '/github-pages',
+  distDir: 'build',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
 }
+module.exports = nextConfig
+// }
