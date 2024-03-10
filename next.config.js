b9fcca6f90da324dd2223607ace3ad6e5a4d61a1
Jonathan Souza Lima <jon.1lima@hotmail.com>
2024-03-10 20:48:11 -0300

    ajuste final

--- next.config.js
+++ next.config.js
@@ -5,33 +5,17 @@ const path = require('path')
  * @type {import('next').NextConfig}
  */
 
-if (process.env.NEXT_PUBLIC_NODE_ENV !== 'local') {
-  console.log('FrontEnd-NextConfig-CI/CD')
-  const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/github-pages',
-    distDir: 'build',
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'src/styles')],
-    },
-  }
-  module.exports = nextConfig
-} else {
-  console.log('Ambiente local vai ser iniciado!')
-  const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/github-pages',
-    distDir: 'build',
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'src/styles')],
-    },
-  }
-  module.exports = nextConfig
+console.log('FrontEnd-NextConfig-CI/CD')
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/github-pages',
+  distDir: 'build',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
 }
+module.exports = nextConfig
