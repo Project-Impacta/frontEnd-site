aefe5b4e344cbf18159308b11660b8887f1f3e8e
Jonathan Souza Lima <jon.1lima@hotmail.com>
2024-03-16 09:00:03 -0300

    ajuste next config para upload no versel

--- next.config.js
+++ next.config.js
@@ -1,21 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { version } = require('./package.json')
 
 /**
  * @type {import('next').NextConfig}
  */
-
 const nextConfig = {
-  extends: ['plugin:@next/next/recommended'],
-  output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  basePath: '/github-pages',
-  distDir: 'build',
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
   },
@@ -26,4 +20,5 @@ const nextConfig = {
     version,
   },
 }
+
 module.exports = nextConfig
