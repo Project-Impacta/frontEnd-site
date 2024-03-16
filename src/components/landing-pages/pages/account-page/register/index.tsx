3b2fca95baf072fc249cf5c6994655d90ab16a00
Jonathan Souza Lima <61327152+JhowSL@users.noreply.github.com>
2024-03-16 02:31:27 -0300

    teste header direto com imagem

--- src/components/landing-pages/pages/account-page/register/index.tsx
+++ src/components/landing-pages/pages/account-page/register/index.tsx
@@ -1,13 +1,14 @@
 import { RegisterForm } from '@/components/api/auth'
 import Head from 'next/head'
 import React from 'react'
+import LogoImpactaStore from '@/components/shared/imagens/logo-impacta'
 
 const RegisterPage = (): JSX.Element => {
   return (
     <>
       <Head>
         <title>Cadastre-se</title>
-        <link rel="icon" href="/favicon.ico" />
+        <LogoImpactaStore />
       </Head>
       <div>
         <RegisterForm />
