3127e09bb459be3bd121f744d5878fcbf8528c0f
JhowSL <jon.1lima@hotmail.com>
2024-05-14 12:44:28 -0300

    ajuste regex email

--- src/lib/utils/form-utils/index.tsx
+++ src/lib/utils/form-utils/index.tsx
@@ -19,7 +19,7 @@ export const validatePasswordsMatch = (password: any, repeatPassword: any) => {
 };
 
 export const validateEmail = (email: string) => {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+  return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
     ? ''
     : 'O e-mail deve conter a seguinte estrutura: example@example.com';
 };
